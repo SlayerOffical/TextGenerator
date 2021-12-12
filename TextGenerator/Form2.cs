8840e1338f60456c0d4172ab8dbbbfd5203913c6
SlayerOffical <90822002+SlayerOffical@users.noreply.github.com>
2021-12-12 12:22:21 -0800

    Add project files.

---
+++ TextGenerator/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextGenerator
+{
+    public partial class Form2 : Form
+    {
+        public static Form2 instance;
+
+        public Form2()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.instance.BackColor = Color.Black;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1.instance.tbf1.ReadOnly = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1.instance.tbf1.ReadOnly = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form1.instance.tbf1.BackColor = Color.White;
+        }
+    }
+}
